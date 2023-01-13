7dbfc1164049602a1b39f7ca522babb4850c7164
BertCasale <bthebest55@gmail.com>
2023-01-13 17:14:20 -0500

    create json file

--- collection.js
+++ collection.js
@@ -1,4 +1,28 @@
 let yourCollection = {};
+//save the session storage to your collection
 Object.keys(sessionStorage).forEach((key) => {
     yourCollection[key] = sessionStorage.getItem(key);
 });
+
+
+
+
+
+//////Import a file
+
+//get the file input
+let fileInput = document.querySelector(".user-file")
+
+fileInput.onchange = (e) => { 
+    let userFile = e.target.files[0]; 
+    console.log(userFile.type)
+    if (userFile.type !== 0){
+
+    }
+}
+
+//on click, take on a file
+fileInput.click();
+
+
+//////Export a file
